172b109341570ba2cf81bb1ed9d4dcbec7461483
Diego Vieira <vieiracdiego@gmail.com>
2022-05-06 20:07:06 -0300

    Add vue slider component

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/rebalancing.js', 'public/js')
+    .js('resources/js/target.js', 'public/js')
+    .vue({ version: 2 })
+    .sass('resources/sass/app.scss', 'public/css');
