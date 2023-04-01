f90d82c9f641d263d8b00778ef26ea383570945d
Diego Vieira <dvieira@inbenta.com>
2023-04-01 14:26:09 -0300

    Add new migrations

--- database/migrations/2023_02_21_135059_create_user_asset_classes_table.php
+++ database/migrations/2023_02_21_135059_create_user_asset_classes_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_asset_classes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('asset_class_id')->constrained('asset_classes')->onDelete('cascade');
+            $table->integer('percentage');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_asset_classes');
     }
 };
