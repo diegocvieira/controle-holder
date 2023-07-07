ca2aee6917317cf737836c44f826c17984ac0809
Diego Vieira <dvieira@inbenta.com>
2023-07-07 08:55:10 -0300

    Add dashboard files

---
+++ resources/js/pages/dashboard/dashboard.js
@@ -0,0 +1,15 @@
+import HeaderComponent from '../../components/header-component.vue';
+
+export default new Vue({
+    el: '#dashboard-page',
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+    },
+    components: {
+        HeaderComponent
+    }
+});
---
+++ resources/views/dashboard/dashboard/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts/master')
+
+@section('content')
+
+<main class="main" id="dashboard-page">
+    <header-component page="dashboard"></header-component>
+
+    <div class="main-content">
+        dashboard
+    </div>
+</main>
+
+@endsection
