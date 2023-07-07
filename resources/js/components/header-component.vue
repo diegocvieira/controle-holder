a55969fe0e517255379c9d3d2e811c7c8c9575d5
Diego Vieira <dvieira@inbenta.com>
2023-07-07 08:54:51 -0300

    Add header logout and improve methods description

--- resources/js/components/header-component.vue
+++ resources/js/components/header-component.vue
@@ -8,7 +8,7 @@
             <nav>
                 <ul>
                     <li v-for="(item, key) in items" :key="key">
-                        <a :href="item.url" class="link" :class="item.is_active == true ? 'is-active' : ''" v-on:click.prevent="openDropdown(item.slug, item.url)" :title="item.name">
+                        <a :href="item.url" class="link" :class="item.is_active == true ? 'is-active' : ''" v-on:click.prevent="handleLinkAction(item.slug, item.url)" :title="item.name">
                             <span class="icon" :class="item.slug" v-html="item.icon"></span>
 
                             {{ item.name }}
@@ -104,7 +104,7 @@
                     {
                         name: 'Rebalanceamento',
                         slug: 'rebalancing',
-                        url: '#',
+                        url: '/dashboard/rebalancing',
                         icon: `<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                                 <path fill="currentColor" d="M96 464v32c0 8.84 7.16 16 16 16h224c8.84 0 16-7.16 16-16V153.25c4.56-2 8.92-4.35 12.99-7.12l142.05 47.63c8.38 2.81 17.45-1.71 20.26-10.08l10.17-30.34c2.81-8.38-1.71-17.45-10.08-20.26l-128.4-43.05c.42-3.32 1.01-6.6 1.01-10.03 0-44.18-35.82-80-80-80-29.69 0-55.3 16.36-69.11 40.37L132.96.83c-8.38-2.81-17.45 1.71-20.26 10.08l-10.17 30.34c-2.81 8.38 1.71 17.45 10.08 20.26l132 44.26c7.28 21.25 22.96 38.54 43.38 47.47V448H112c-8.84 0-16 7.16-16 16zM0 304c0 44.18 57.31 80 128 80s128-35.82 128-80h-.02c0-15.67 2.08-7.25-85.05-181.51-17.68-35.36-68.22-35.29-85.87 0C-1.32 295.27.02 287.82.02 304H0zm56-16l72-144 72 144H56zm328.02 144H384c0 44.18 57.31 80 128 80s128-35.82 128-80h-.02c0-15.67 2.08-7.25-85.05-181.51-17.68-35.36-68.22-35.29-85.87 0-86.38 172.78-85.04 165.33-85.04 181.51zM440 416l72-144 72 144H440z"></path>
                             </svg>`,
@@ -120,17 +120,34 @@
                             </svg>`,
                         dropdown_items: [],
                         is_active: false
+                    },
+                    {
+                        name: 'Sair',
+                        slug: 'logout',
+                        url: '/api/auth/logout',
+                        icon: `<svg viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M16,17V14H9V10H16V7L21,12L16,17M14,2A2,2 0 0,1 16,4V6H14V4H5V20H14V18H16V20A2,2 0 0,1 14,22H5A2,2 0 0,1 3,20V4A2,2 0 0,1 5,2H14Z"></path>
+                            </svg>`,
+                        dropdown_items: [],
+                        is_active: false
                     }
                 ]
             }
         },
         methods: {
-            openDropdown: function(linkSlug, linkUrl) {
-                if (linkUrl != '#') {
-                    window.open(linkUrl, '_self')
+            handleLinkAction: function(linkSlug, linkUrl) {
+                if (linkSlug === 'logout') {
+                    axios.post(linkUrl, []).then(response => {
+                        window.open('/login', '_self');
+                    }).catch(error => console.log(error));
+                } else if (linkUrl != '#') {
+                    window.open(linkUrl, '_self');
                 }
 
                 this.openOrHideAside(linkUrl);
+                this.openDropdown(linkSlug);
+            },
+            openDropdown: function(linkSlug) {
                 this.choice = this.choice === linkSlug ? '' : linkSlug;
             },
             isActiveMenu: function(value) {
