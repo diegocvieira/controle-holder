243b578b680df282f39bedaa0f8944dcbc156ade
Diego Vieira <dvieira@inbenta.com>
2023-08-27 10:26:11 -0300

    Fix translations

--- lang/pt_BR/asset.php
+++ lang/pt_BR/asset.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'asset_not_found' => 'Ativo não encontrado. Entre em contato <a href="#">por aqui</a> e peça a inclusão dele no sistema..'
+    'asset_not_found' => 'Ativo não encontrado. Entre em contato <a href="#">por aqui</a> e peça a inclusão dele no sistema.'
 ];
