f84429b12355d803cd09863073076fe1b505e823
Diego Vieira <dvieira@inbenta.com>
2023-10-22 11:29:09 -0300

    Validate dashboard

--- resources/views/dashboard/dashboard/index.blade.php
+++ resources/views/dashboard/dashboard/index.blade.php
@@ -14,23 +14,30 @@
         'title' => 'Gráficos'
     ])
 
-    <nav class="is-flex mb-20">
-        <ul class="is-flex">
-            <li v-for="filterOption in filterOptions">
-                <a href="#" @click.prevent="applyFilter(filterOption.slug)" :class="'link ' + (filterOption.slug === filter ? 'is-active' : '')">@{{ filterOption.name }}</a>
-            </li>
-        </ul>
-    </nav>
+    <div v-if="wallet.length === 0">
+        @include ('partials.empty-results', [
+            'message' => 'Você ainda não tem nenhum ativo cadastrado...'
+        ])
+    </div>
+    <div v-else>
+        <nav class="is-flex mb-20">
+            <ul class="is-flex">
+                <li v-for="filterOption in filterOptions">
+                    <a href="#" @click.prevent="applyFilter(filterOption.slug)" :class="'link ' + (filterOption.slug === filter ? 'is-active' : '')">@{{ filterOption.name }}</a>
+                </li>
+            </ul>
+        </nav>
 
-    <div class="charts">
-        <div class="chart">
-            <pie-component :data="chartCurrent"></pie-component>
-        </div>
+        <div class="charts">
+            <div class="chart">
+                <pie-component :data="chartCurrent"></pie-component>
+            </div>
 
-        <hr class="line vertical">
+            <hr class="line vertical">
 
-        <div class="chart">
-            <pie-component :data="chartIdeal"></pie-component>
+            <div class="chart">
+                <pie-component :data="chartIdeal"></pie-component>
+            </div>
         </div>
     </div>
 @endsection
