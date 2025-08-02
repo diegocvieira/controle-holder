34146b997fabddc5dde5a0b62fdef4cd9701a236
Diego Vieira <dvieira@inbenta.com>
2025-08-02 17:41:53 -0300

    Add link

--- resources/js/views/auth/Login.vue
+++ resources/js/views/auth/Login.vue
@@ -1,9 +1,9 @@
 <template>
     <main id="login-page" class="is-relative h-screen is-flex">
-        <a href="#" class="back-link">
+        <router-link :to="{ name: 'home' }" class="back-link">
             <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M13.25 8.75L9.75 12L13.25 15.25" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path></svg>
             Página inicial
-        </a>
+        </router-link>
 
         <div class="container max-w-450">
             <div class="">
